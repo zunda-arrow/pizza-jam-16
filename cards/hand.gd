9d0e1395443ffbb71f8d55c0cb0ad58b2cac6bae
Zunda <mailme@jacobhaussman.online>
2026-03-13 20:21:07 -0400

    create card and hand nodes

---
+++ cards/hand.gd
@@ -0,0 +1,73 @@
+@tool
+
+extends Node2D
+
+var _card_scene = preload("res://cards/Card.tscn")
+
+var cards = []
+var card_scenes = []
+
+signal card_clicked(i: int)
+
+@export var position_along_circle_of_radius = 2000
+@export var distance_between_cards = 200
+@export var animation_speed = 30
+
+func _ready():
+	for i in range(6):
+		var next_card = _card_scene.instantiate()
+		card_scenes.push_back(next_card)
+		%Cards.add_child(next_card)
+		
+		next_card.on_clicked.connect(func ():
+			card_clicked.emit(i)
+		)
+
+	positon_cards(-1)
+
+func _process(delta: float) -> void:
+	positon_cards(delta)
+
+func positon_cards(delta):
+	var size_of_circle = position_along_circle_of_radius
+
+	var selected_card_index = -1
+	
+	for i in range(len(card_scenes)):
+		if card_scenes[i].hovered:
+			selected_card_index = i
+
+	for i in range(len(card_scenes)):
+		var card: Node2D = card_scenes[i]
+		var distance_from_center = len(card_scenes) / 2. - i - 0.5
+
+		var x_positon = -distance_from_center * distance_between_cards
+		# x^2 + y^2 = r^2
+		# y^2 = r^2 - x^2
+		# y = sqrt(r^2 - x^2)
+		var y_position = size_of_circle - sqrt(size_of_circle ** 2 - x_positon ** 2)
+		var target_rotation = 0.
+		if x_positon != 0:
+			target_rotation = tan(y_position / x_positon)
+		
+		var target_scale = Vector2(1, 1)
+
+		if selected_card_index == i:
+			target_scale = Vector2(1.5, 1.5)
+			target_rotation = 0
+			y_position = 0
+
+		if selected_card_index != -1:
+			if selected_card_index < i:
+				x_positon += 50
+			if selected_card_index > i:
+				x_positon -= 50
+
+		var lerp_amount = animation_speed * delta
+		if delta == -1:
+			lerp_amount = 1
+
+		card.position.x = lerp(card.position.x, float(x_positon), lerp_amount)
+		card.position.y = lerp(card.position.y, float(y_position), lerp_amount)
+		card.rotation = lerp(card.rotation, float(target_rotation), lerp_amount)
+		card.scale = lerp(card.scale, target_scale, lerp_amount)	
