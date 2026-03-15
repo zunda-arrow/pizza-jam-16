75185399c6bfe15cd7c70b84e3da10f62843502f
Tyrannicodin <tyrannicodin@gmail.com>
2026-03-15 00:39:49 +0000

    Terrain gen main (#8)
    * Add random generation of world
    
    Dirt tilemap courtesy of Kitty.
    
    (cherry picked from commit 07353568dfec271c98b29bfd3396c816c0e959c6)
    
    # Conflicts:
    #	terrain/terrain.tscn
    
    * Combine generator and tile_map_layers into terrain_manager

--- terrain/terrain_manager.gd
+++ terrain/terrain_manager.gd
@@ -1,40 +1,71 @@
+# Handles terrain generation
 extends Node2D
+
+@export var initial_seed = 0 # The noise seed on ready. If set to 0, random seed.
+
+@export var noise: FastNoiseLite # The noise we will use to generate the terrain
+@export var block_threshold: float = 0.5 # Threshold to place a block (less than)
+@export var unbreakable_threshold: float = 0.0 # Threshold to place an unbreakable block #TODO: Implement
 
 var structure_scene = preload("res://terrain/structure.tscn")
 var example_resource: StructureResource = preload("res://resources/structures/example.tres")
 
-@export var placing_build = true
+var placing_build = true
+
+@onready var tilemap: TileMapLayer = %GroundMap
+
+func _ready():
+	if initial_seed == 0:
+		initial_seed = randi()
+	generate(initial_seed)
+
+func generate(new_seed) -> void:
+	tilemap.clear()
+	noise.seed = new_seed
+
+	await get_tree().process_frame
+
+	var set_cells = []
+	for y in 100:
+		for x in 100:
+			#TODO: Can probably do this better.
+			if get_cell(x, y):
+				set_cells.append(Vector2i(x, y))
+	tilemap.set_cells_terrain_connect(set_cells, 0, 0)
+
+func get_cell(x: int, y: int) -> bool: # Check if there is a cell here
+	return absf(noise.get_noise_2d(x, y)) > block_threshold
 
 # Radius is a square radius
-func destroy(position: Vector2i, diameter: int):
+func destroy(pos: Vector2i, diameter: int):
 	var radius = diameter / 2.
-	var cell_coordinate_center = $TileMapLayer.local_to_map(Vector2i(position.x,position.y+40))
+	var cell_coordinate_center = tilemap.local_to_map(Vector2i(pos.x, pos.y+40))
 	
 	for x in range(ceil(cell_coordinate_center.x-radius),ceil(cell_coordinate_center.x+radius)):
 		for y in range(ceil(cell_coordinate_center.y-radius),ceil(cell_coordinate_center.y+radius)):
-			if $TileMapLayer.get_cell_source_id(Vector2(x,y)) >= 0:
-				$TileMapLayer.erase_cell(Vector2(x,y))
+			if tilemap.get_cell_source_id(Vector2(x,y)) >= 0:
+				tilemap.erase_cell(Vector2(x,y))
 
-func show_selector(position: Vector2i, size: Array[Rect2i]):
+func show_selector(pos: Vector2i, size: Array[Rect2i]):
 	$Selection.clear()
-	var cell_coordinate_center = $Selection.local_to_map(Vector2i(position.x,position.y+40))
+	var cell_coordinate_center = $Selection.local_to_map(Vector2i(pos.x, pos.y+40))
 	
 	for rect in size:
 		for x in range(ceil(cell_coordinate_center.x+rect.position.x-rect.size.x),ceil(cell_coordinate_center.x+rect.position.x++1+rect.size.x)):
 			for y in range(ceil(cell_coordinate_center.y+rect.position.y-rect.size.y),ceil(cell_coordinate_center.y+rect.position.y+1+rect.size.y)):
-				if $TileMapLayer.get_cell_source_id(Vector2(x,y)) >= 0:
+				if tilemap.get_cell_source_id(Vector2(x,y)) >= 0:
 					$Selection.set_cell(Vector2(x,y), 0, Vector2(1,0), 0)
 				else:
 					$Selection.set_cell(Vector2(x,y), 0, Vector2(0,0), 0)
-					
-func place_build(position: Vector2i, structure: StructureResource):
-	var cell_coordinate_center = $Selection.local_to_map(Vector2i(position.x,position.y+40))
+
+func place_build(pos: Vector2i, structure: StructureResource):
+	var cell_coordinate_center = $Selection.local_to_map(Vector2i(pos.x,pos.y+40))
 	var can_place = true
 	
 	for rect in structure.size:
 		for x in range(ceil(cell_coordinate_center.x+rect.position.x-rect.size.x),ceil(cell_coordinate_center.x+rect.position.x++1+rect.size.x)):
 			for y in range(ceil(cell_coordinate_center.y+rect.position.y-rect.size.y),ceil(cell_coordinate_center.y+rect.position.y+1+rect.size.y)):
-				if $TileMapLayer.get_cell_source_id(Vector2(x,y)) >= 0:
+				if tilemap.get_cell_source_id(Vector2(x,y)) >= 0:
 					can_place = false
 					
 	if (can_place):
