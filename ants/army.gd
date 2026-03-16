e61f58dc1aa915f370e72a3d94dfb236839a62f5
Zunda <mailme@jacobhaussman.online>
2026-03-15 20:18:44 -0400

    now they pick clockwise and counter clockwise

--- ants/army.gd
+++ ants/army.gd
@@ -4,6 +4,8 @@ var ant = preload("res://ants/Ant.tscn")
 
 @export var is_grid_cell_filled: Callable
 
+var ants: Array[Ant] = []
+
 func _ready() -> void:
 	is_grid_cell_filled = default_is_grid_cell_filled
 
@@ -23,8 +25,7 @@ func spawn_ant():
 	var random_test_ant: Ant = ant.instantiate()
 	%Ants.add_child(random_test_ant)
 	random_test_ant.grid_position = loop.pick_random()
-	
-	random_test_ant.move_to_tile(get_path_to_cell(loop, random_test_ant.grid_position, loop.pick_random()))
+	ants.push_back(random_test_ant)
 
 func get_loop(pos: Vector2i) -> Array[Vector2i]:
 	var forward = Vector2(1, 0)
@@ -62,15 +63,39 @@ func get_path_to_cell(loop: Array[Vector2i], from: Vector2i, to: Vector2i) -> Ar
 	
 	var path: Array[Vector2i]
 	
-	var i = index_of_from
-	while i != index_of_to:
-		path.push_back(loop[i])
-		i+=+1
-		if i > len(path):
-			
-			i = 0
+	if abs(index_of_to - index_of_from) < len(loop) - index_of_to - index_of_from:
+		var i = index_of_from
+		while i != index_of_to:
+			path.push_back(loop[i])
+			i+=+1
+			if i >= len(loop):
+				i = 0
+	else:
+		var i = index_of_from
+		while i != index_of_to:
+			path.push_back(loop[i])
+			i-=1
+			if i < 0:
+				i = len(loop) - 1
+
+	path.push_back(loop[index_of_to])
 	
 	return path
 
 func default_is_grid_cell_filled(cell: Vector2) -> bool:
 	return $TileMap.get_cell_tile_data(0, cell) != null
+
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+
+
+		var mouse_cell = floor(get_global_mouse_position() / 32)
+		var ant = ants[0]
+		
+		var ant_loop = get_loop(ant.grid_position)
+		if mouse_cell not in ant_loop:
+			return
+		
+		ant.move_to_tile(get_path_to_cell(ant_loop, ant.grid_position, mouse_cell))
