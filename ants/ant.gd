2ead7ff3bd70f0a31c12e5144b7a9a7ac094d246
Zunda <mailme@jacobhaussman.online>
2026-03-15 20:10:22 -0400

    ANT MOVEMENT

--- ants/ant.gd
+++ ants/ant.gd
@@ -1,5 +1,22 @@
 extends Sprite2D
+class_name Ant
 
+var following_path: Array[Vector2i] = []
+var timer = 0
 
-func move_to_tile(tile: Vector2i):
-	pass
+var grid_position: Vector2i:
+	set(pos):
+		self.position = (pos * 32) + Vector2i(16, 16)
+	get():
+		return Vector2i(self.position / 32)
+
+func move_to_tile(along_path: Array[Vector2i]):
+	following_path = along_path
+
+func _process(delta: float) -> void:
+	timer += delta
+	
+	if timer >= .1:
+		timer = 0
+		if len(following_path) > 0:
+			grid_position = following_path.pop_at(0)
--- ants/army.gd
+++ ants/army.gd
@@ -1,3 +1,76 @@
 extends Node2D
 
 var ant = preload("res://ants/Ant.tscn")
+
+@export var is_grid_cell_filled: Callable
+
+func _ready() -> void:
+	is_grid_cell_filled = default_is_grid_cell_filled
+
+	var loop = get_loop(Vector2i(0, 3))
+
+	for i in range(len(loop)):
+		var m = $Marker.duplicate()
+		add_child(m)
+		m.show()
+		m.text = str(i)
+		m.position = loop[i] * 32 + Vector2i(16, 16)
+
+	spawn_ant()
+
+func spawn_ant():
+	var loop = get_loop(Vector2i(0, 3))
+	var random_test_ant: Ant = ant.instantiate()
+	%Ants.add_child(random_test_ant)
+	random_test_ant.grid_position = loop.pick_random()
+	
+	random_test_ant.move_to_tile(get_path_to_cell(loop, random_test_ant.grid_position, loop.pick_random()))
+
+func get_loop(pos: Vector2i) -> Array[Vector2i]:
+	var forward = Vector2(1, 0)
+	if is_grid_cell_filled.call(pos + Vector2i(1, 0)):
+		forward = Vector2(0, -1)
+	if is_grid_cell_filled.call(pos + Vector2i(0, -1)):
+		forward = Vector2(-1, 0)
+	if is_grid_cell_filled.call(pos + Vector2i(-1, 0)):
+		forward = Vector2(0, 1)
+
+	var walkable_cells: Array[Vector2i] = []
+	
+	while pos not in walkable_cells:
+		var under = Vector2i(Vector2(forward).rotated(PI / 2))
+
+		if is_grid_cell_filled.call(pos + under) == false:
+			forward = under
+			pos = pos + under
+			continue
+
+		if is_grid_cell_filled.call(pos + Vector2i(forward)) == true:
+			forward = Vector2i(Vector2(forward).rotated(-PI / 2))
+			walkable_cells.push_back(pos)
+			pos += Vector2i(forward)
+
+		if is_grid_cell_filled.call(pos + under) == true:
+			walkable_cells.push_back(pos)
+			pos += Vector2i(forward)
+
+	return walkable_cells
+
+func get_path_to_cell(loop: Array[Vector2i], from: Vector2i, to: Vector2i) -> Array[Vector2i]:
+	var index_of_from = loop.find(from)
+	var index_of_to = loop.find(to)
+	
+	var path: Array[Vector2i]
+	
+	var i = index_of_from
+	while i != index_of_to:
+		path.push_back(loop[i])
+		i+=+1
+		if i > len(path):
+			
+			i = 0
+	
+	return path
+
+func default_is_grid_cell_filled(cell: Vector2) -> bool:
+	return $TileMap.get_cell_tile_data(0, cell) != null
