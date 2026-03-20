72c4d6754d6159328a58b06c3a51249df29667c8
Zunda <mailme@jacobhaussman.online>
2026-03-20 18:24:28 -0400

    add various drill shapes

--- cards/all_cards.gd
+++ cards/all_cards.gd
@@ -1,23 +1,24 @@
 extends Node
 
 var resources: Array[CardResource] = [
-	#load("res://resources/cards/fungus_bar.tres"),
-	#load("res://resources/cards/breakfast.tres"),
-	#load("res://resources/cards/beam_drill.tres"),
-	#load("res://resources/cards/dirt_nap.tres"),
-	#load("res://resources/cards/super_drill.tres"),
-	#load("res://resources/cards/drill.tres"),
-	#load("res://resources/cards/big_drill.tres"),
-	#load("res://resources/cards/super_buff.tres"),
-	#load("res://resources/cards/bulldozer.tres"),
-	#load("res://resources/cards/brainstorm.tres"),
-	#load("res://resources/cards/bridge.tres"),
-	#load("res://resources/cards/ladder.tres"),
+	load("res://resources/cards/fungus_bar.tres"),
+	load("res://resources/cards/breakfast.tres"),
+	load("res://resources/cards/beam_drill.tres"),
+	load("res://resources/cards/dirt_nap.tres"),
+	load("res://resources/cards/super_drill.tres"),
+	load("res://resources/cards/drill.tres"),
+	load("res://resources/cards/big_drill.tres"),
+	load("res://resources/cards/super_buff.tres"),
+	load("res://resources/cards/bulldozer.tres"),
+	load("res://resources/cards/brainstorm.tres"),
+	load("res://resources/cards/bridge.tres"),
+	load("res://resources/cards/ladder.tres"),
 	load("res://resources/cards/laser_drill.tres"),
 	load("res://resources/cards/heart.tres"),
 	load("res://resources/cards/shroom_drill.tres"),
 	load("res://resources/cards/spear_drill.tres"),
 	load("res://resources/cards/club_drill.tres"),
+	load("res://resources/cards/diamond_drill.tres"),
 ]
 
 var cards = resources.map(func(card): return card.new())
