93d4971fdc3f6a99f4efadebaa1703fd699109e4
Zunda <mailme@jacobhaussman.online>
2026-03-20 21:16:00 -0400

    add daily goal and toolbar

--- game/game.gd
+++ game/game.gd
@@ -8,6 +8,11 @@ signal shop_start
 var deck: Array[CardResource.Card] = []
 var money := 0
 
+var day = 1
+
+func calculate_daily_goal(day_number: int):
+	return 10*day_number
+
 func loop_music():
 	$Music.play()
 	await get_tree().create_timer(88.63).timeout
@@ -25,13 +30,19 @@ func _ready() -> void:
 
 func start_game() -> void:
 	day_start.emit(deck) # Day start also starts a turn.
+	
+	%Toolbar.daily_goal = calculate_daily_goal(day)
 
 func on_day_end() -> void:
 	shop_start.emit()
 	get_tree().paused = true
+	day += 1
+	%Toolbar.daily_goal = calculate_daily_goal(day)
+	
 
 func on_money_earned(value: int) -> void:
 	money += value
+	%Toolbar.money = money
 
 func get_money() -> int:
 	return money
