76f57d4c025910bfbbcfa7ab0b6234be9faf267c
Zunda <mailme@jacobhaussman.online>
2026-03-20 22:01:15 -0400

    new toolbar

--- game/toolbar.gd
+++ game/toolbar.gd
@@ -6,7 +6,22 @@ extends Node2D
 @export var daily_goal: int
 @export var total_days: int = 20
 
+@export var turn: int
+
+@export var in_game: bool = false
+
+@export var ant_count: int
+@export var energy_count: int
+
 
 func _process(delta: float) -> void:
 	%Money.text = str(money) + "/" + str(daily_goal)
-	%Day.text = str(day) + "/" + str(total_days)
+	%Day.text = "Day " + str(day) + "/" + str(total_days)
+	%Turn.text = "Turn " + str(turn + 1) + "/5"
+
+	if not in_game:
+		%Energy.text = str(0)
+		%Ants.text = str(0)
+	else:
+		%Energy.text = str(energy_count)
+		%Ants.text = str(ant_count)
