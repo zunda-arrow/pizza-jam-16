df08b0847f5d7992263eff4f2dd3f9ca3300d99f
Benji42 <cheezyanimations@gmail.com>
2026-03-22 14:39:46 -0400

    reward delay

--- cards/card_reward.gd
+++ cards/card_reward.gd
@@ -5,7 +5,10 @@ func _ready():
 	hide()
 
 func show_cards(cards: Array[CardResource.Card]) -> void:
+	$Panel.hide()
+	await get_tree().create_timer(2.0).timeout
 	show()
+	$Panel.show()
 	$CardList.show_cards(cards)
 
 func on_respond() -> void:
