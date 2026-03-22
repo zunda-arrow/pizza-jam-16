1b547c0c7fe63feb08057b33bbebaf7a28b469c6
Jacob Haussman <mailme@jacobhaussman.online>
2026-03-22 02:19:51 -0400

    add prices to tooltips (#110)

--- include/tooltip/tooltip_panel.gd
+++ include/tooltip/tooltip_panel.gd
@@ -3,7 +3,9 @@ extends Control
 
 @export var tooltip_name: String = "Name"
 @export_multiline var tooltip_description: String = "Example tooltip description"
+@export var tooltip_cost: int = 0
 
 func _process(_delta) -> void:
 	%TooltipName.text = tooltip_name
 	%TooltipDescription.text = tooltip_description
+	%Cost.text = str(tooltip_cost)
